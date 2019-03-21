92f0279c9eceba6048feb4d7452bad0e873ed815
NAKNAO-nnct <totoro.windows@outlook.com>
2019-03-21 16:18:11 +0900

    add mongo db conncection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* userlist htmlページを作成 */
+router.get("/userlist", function(req, res) {
+  var db = req.db;
+  var collection = db.get("stdApp");
+  collection.find({}, {}, function(e, docs) {
+    res.render("userlist", {
+      title: "user list",
+      userlist: docs
+    });
+  });
 });
 
 module.exports = router;
